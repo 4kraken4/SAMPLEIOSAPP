eeb7c82461f7d099bac5621876120e79a8996532
kraken <warunamadushanka456@gmail.com>
2023-12-14 18:08:54 +0530

    Initial Commit

---
+++ SAMPLEIOSAPP/SAMPLEIOSAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  SAMPLEIOSAPPApp.swift
+//  SAMPLEIOSAPP
+//
+//  Created by Mangi Wimalaweera on 2023-12-14.
+//
+
+import SwiftUI
+
+@main
+struct SAMPLEIOSAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
